c7df8a6541b20d8c880c374d0813e6b6829f8518
andre <a.ekani@gmail.com>
2024-04-23 12:27:44 +0200

    update

--- src/services/explorer.service.spec.ts
+++ src/services/explorer.service.spec.ts
@@ -1,14 +1,19 @@
 import { Test } from '@nestjs/testing';
 import { ExplorerService } from './explorer.service';
+import { MetadataScanner } from '@nestjs/core/metadata-scanner';
 
 describe('GIVEN ExplorerService', () => {
   let explorerService: ExplorerService;
 
   beforeAll(async () => {
     const module = await Test.createTestingModule({
-      providers: [ExplorerService],
+      providers: [ExplorerService, MetadataScanner],
     }).compile();
 
     explorerService = module.get(ExplorerService);
+  });
+
+  test('WHEN ', () => {
+    expect(true).toBe(true);
   });
 });
